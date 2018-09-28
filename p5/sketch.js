ba0e8fca2ea21309f7a127cd655149687bf5b4b3
RookeryPhysics <43014261+RookeryPhysics@users.noreply.github.com>
2018-09-28 13:14:37 -0600

    Initial commit new game

---
+++ p5/sketch.js
@@ -0,0 +1,25 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let shipX;
+let shipY;
+let dx;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0,140,140);
+  createShip();
+  cureCancer();
+  solveWorldHunger();
+  defeatISIS();
+  giveObamaAThirdTerm();
+  spawnIceberg();
+  sinkTitanic();
+}
