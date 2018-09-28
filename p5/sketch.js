268e429d263221be817cd990005c3be6eaf49abb
RookeryPhysics <43014261+RookeryPhysics@users.noreply.github.com>
2018-09-28 14:11:39 -0600

    Started

--- p5/sketch.js
+++ p5/sketch.js
@@ -7,19 +7,45 @@
 
 let shipX;
 let shipY;
-let dx;
+let state;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  shipX = width/2;
+  shipY = height/2;
+  state = 1;
 }
 
 function draw() {
   background(0,140,140);
-  createShip();
-  cureCancer();
-  solveWorldHunger();
-  defeatISIS();
-  giveObamaAThirdTerm();
-  spawnIceberg();
-  sinkTitanic();
+  if(state === 1){
+    controlShip();
+    createWaves();
+  }
+}
+
+function controlShip(){
+  fill(255,0,0);
+  if(!keyIsDown(37) && !keyIsDown(39)){
+    rect(shipX, shipY, 10, 30);
+  }
+  else if(keyIsDown(37)){
+    shipX -= 4;
+    rect(shipX, shipY, 30, 10);
+  }
+  else if(keyIsDown(39)){
+    shipX += 4;
+    rect(shipX, shipY, 30, 10);
+  }
+  if(keyIsDown(38) && !keyIsDown(37) && !keyIsDown(39)){
+    shipY -= 2;
+    rect(shipX, shipY, 10, 30);
+  }
+  else if(keyIsDown(40) && !keyIsDown(37) && !keyIsDown(39)){
+    shipY += 5;
+    rect(shipX, shipY, 10, 30);
+  }
+}
+
+function createWaves(){
 }
