b6fe955b088463db69f20545d82a2ef22d8e6570
RookeryPhysics <43014261+RookeryPhysics@users.noreply.github.com>
2018-10-29 14:12:31 -0600

    new tile game

---
+++ p5-4/sketch.js
@@ -0,0 +1,144 @@
+// Memory Tile Game
+// Michael McGee
+// 29/10/2018
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+// do this ^^^
+
+let state;
+let randomArray = [];
+let newArray = [];
+let division;
+let randomness;
+let message;
+let hasNotStarted;
+let time;
+
+function setup() {
+  createCanvas(600, 600);
+  division = 4;
+  randomness = 0.5;
+  state = 0;
+  hasNotStarted = true;
+  message = true;
+  for(let i = 0; i < division; i++){
+    randomArray[i] = [];
+    newArray[i] = [];
+  }
+  createBlankArray();
+  generate();
+}
+
+function keyTyped(){
+  if(state === 2 && keyCode === 13){
+    state = 3;
+  }
+  if(state === 0 && keyCode === 13){
+    state = 1;
+  }
+}
+
+function draw() {
+  loadState();
+}
+
+function mousePressed(){
+  if(state === 2){
+    let x = floor(mouseX / (width / division));
+    let y = floor(mouseY / (height / division));
+    if (newArray[x][y] === 1){
+      newArray[x][y] = 0;
+    }
+    else if (newArray[x][y] === 0){
+      newArray[x][y] = 1;
+    }
+  }
+}
+
+function createBlankArray(){
+  for(let x = 0; x < division; x++){
+    for(let y = 0; y < division; y++){
+      newArray[x][y] = 0;
+    }
+  }
+}
+
+function loadState(){
+  if(state === 0){
+    hasNotStarted = true;
+    startScreen();
+  }
+  if(state === 1){
+    if(hasNotStarted){
+      startTheClock();
+    }
+    while(millis() < time + 5000){
+      displayRandomGrid(); //FIND A WAY TO DO THIS FOR A CERTAIN AMOUNT OF TIME
+    }
+  }
+  if(state === 2){
+    if(message){
+      alert("Press enter to submit.");
+    }
+    message = false;
+    displayNewArray();
+    //LOAD AN EMPTY NEW GRID HERE AND DISPLAY IT UNTIL PLAYER SUBMITS
+    //LET THEM DRAW ON IT TO TRY AND REPLICATE ORIGINAL
+  }
+  if(state === 3){
+    //SCORE THE PLAYER IN THIS STATE
+  }
+}
+
+//displays the start screen, options, and explains how to play the game
+function startScreen(){
+
+}
+
+function startTheClock(){
+  time = millis();
+  hasNotStarted = false;
+}
+
+function generate(){
+  for(let x = 0; x < division; x++){
+    for (let y = 0; y < division; y++){
+      randomArray[x][y] = random(0,1);
+      if(randomArray[x][y] < randomness){
+        randomArray[x][y] = 0;
+      }
+      else if(randomArray[x][y] >= randomness){
+        randomArray[x][y] = 1;
+      }
+    }
+  }
+}
+
+function displayRandomGrid(){
+  for(let w = 0; w < division; w++){
+    for(let e = 0; e < division; e++){
+      if(randomArray[w][e] === 1){
+        fill(0,255,0);
+      }
+      else{
+        fill(37,14,3);
+      }
+      rect(w*(width/division),e*(height/division),width/division,height/division);
+    }
+  }
+}
+
+function displayNewArray(){
+  for(let w = 0; w < division; w++){
+    for(let e = 0; e < division; e++){
+      if(newArray[w][e] === 1){
+        fill(0,255,0);
+      }
+      else{
+        fill(37,14,3);
+      }
+      rect(w*(width/division), e*(height/division), width/division, height/division);
+    }
+  }
+}
