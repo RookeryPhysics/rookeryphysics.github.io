24e69b3e17a2232c411601fc5c20efd6e9447797
Michael O. McGee <mikedangermcgee@gmail.com>
2025-12-27 06:11:14 -0600

    auto LAN IP

--- server/server.js
+++ server/server.js
@@ -84,7 +84,23 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
+const os = require('os');
+
+function getLocalIpAddress() {
+    const interfaces = os.networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name]) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return 'localhost';
+}
+
 server.listen(PORT, '0.0.0.0', () => {
+    const localIp = getLocalIpAddress();
     console.log(`Server running on port ${PORT}`);
-    console.log(`LAN Access: http://172.16.1.218:${PORT}`);
+    console.log(`- Local/LAN Access: http://${localIp}:${PORT}`);
+    console.log(`- Internet Access:  http://YOUR_PUBLIC_IP:${PORT} (Requires Port Forwarding)`);
 });
